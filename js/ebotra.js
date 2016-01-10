ba79b105ca302645f9da3c86a51d7cd924d02eda
Arjun Kapoor <arjunkapoor.nsit@gmail.com>
2016-01-10 19:01:16 +0530

    pages connected

--- js/ebotra.js
+++ js/ebotra.js
@@ -16,4 +16,33 @@
     $( "#keyword_input" ).autocomplete({
       source: availableTags
     });
+      $( "#keyword_input1" ).autocomplete({
+      source: availableTags
+    });
+        $( "#keyword_input2" ).autocomplete({
+      source: availableTags
+    });
+
+  $( "#keyword_input3" ).autocomplete({
+      source: availableTags
+    });
+
+
+    $("#keyword_input").keyup(function(event){
+    if(event.keyCode == 13){
+      window.location.replace("search.html");
+    }
+     $("#keyword_input1").keyup(function(event){
+    if(event.keyCode == 13){
+      window.location.replace("search.html");
+    }
+     $("#keyword_input2").keyup(function(event){
+    if(event.keyCode == 13){
+      window.location.replace("search.html");
+    }
+     $("#keyword_input3").keyup(function(event){
+    if(event.keyCode == 13){
+      window.location.replace("search.html");
+    }
+});
   }
